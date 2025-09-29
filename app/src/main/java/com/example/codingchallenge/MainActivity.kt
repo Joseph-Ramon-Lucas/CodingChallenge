831f984db6be67217d485f044291191ade2ab167
Joseph-Ramon-Lucas <joseph.ramon.lucas@gmail.com>
2025-09-29 10:44:47 -0400

    adding text display

--- app/src/main/java/com/example/codingchallenge/MainActivity.kt
+++ app/src/main/java/com/example/codingchallenge/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.codingchallenge
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.codingchallenge.ui.theme.CodingChallengeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +32,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CodingChallengeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
                 CountingChallengeApp()
+                }
             }
         }
     }
@@ -42,15 +49,19 @@ fun CounterDisplayWithButton(modifier: Modifier = Modifier) {
     Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
 
 
-        Surface() {
-            Text("count is: $counter")
-        }
+        Text("count is: $counter",
+            Modifier.wrapContentSize(Alignment.Center))
 
 
-        Button(onClick = {counter += 1}) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Button(onClick = {counter += 1},
+            modifier = modifier.padding(2.dp)) {
             Text("Tap Me")
         }
     }
+
 }
 
 @Preview(showBackground = true)
